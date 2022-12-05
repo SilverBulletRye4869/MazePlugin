fff6ec85d50990abddf12509dff91ac7dd4e509e
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-12-05 21:32:13 +0900

    Tab

---
+++ src/main/java/silverassist/mazecreate/Tab.java
@@ -0,0 +1,34 @@
+package silverassist.mazecreate;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Tab implements TabCompleter {
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if(args.length==1){
+            return List.of("wand","create");
+        }
+        else if(args.length==2){
+            if(!args[0].equals("create"))return null;
+            List<String> material = new ArrayList<>();
+            Arrays.asList(Material.values()).forEach(m -> {
+                if(!m.isBlock())return;
+                String name = m.name();
+                if(name.indexOf(args[1])!=0)return;
+                material.add(name);
+            });
+            return material;
+        }
+        return null;
+    }
+}
