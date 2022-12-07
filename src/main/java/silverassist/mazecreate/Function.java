d850ea02a799e9861b8416c2a981a0bbef91b7e1
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-12-07 19:45:02 +0900

    setblock関数の移動

--- src/main/java/silverassist/mazecreate/Function.java
+++ src/main/java/silverassist/mazecreate/Function.java
@@ -1,6 +1,11 @@
 package silverassist.mazecreate;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class Function {
     public static final String PREFIX = "§b§l[§e§lMazeCreate§b§l]";
@@ -13,6 +18,14 @@ public class Function {
 
     public static void broadCast(String s){
         MazeCreate.plugin.getServer().broadcastMessage(PREFIX+s);
+    }
+
+    public static void setBlock(World w, float[] base, List<Integer> loc, int height, Material m){
+        int x = loc.get(0);
+        int z = loc.get(1);
+        for(int k = 0;k<=height;k++){
+            new Location(w,base[0]+x,base[1]+k,base[2]+z).getBlock().setType(m);
+        }
     }
 
 }
