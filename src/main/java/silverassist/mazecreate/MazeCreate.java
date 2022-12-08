7ae83ad8fce780d60da84d302d10b3d76c981465
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-12-08 18:07:14 +0900

    yml対応化

--- src/main/java/silverassist/mazecreate/MazeCreate.java
+++ src/main/java/silverassist/mazecreate/MazeCreate.java
@@ -4,20 +4,31 @@ import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MazeCreate extends JavaPlugin {
-    public static JavaPlugin plugin = null;
+    private static JavaPlugin plugin = null;
+    private static CustomConfig data = null;
+    private static CustomConfig time = null;
     @Override
     public void onEnable() {
         plugin = this;
+        plugin.saveDefaultConfig();
+        data = new CustomConfig(this,"data.yml");
+        time = new CustomConfig(this,"time.yml");
+        data.saveDefaultConfig();
+        time.saveDefaultConfig();
         // Plugin startup logic
         PluginCommand command = getCommand("maze");
         command.setExecutor(new Command());
         command.setTabCompleter(new Tab());
         this.getServer().getPluginManager().registerEvents(new RangeSet(),this);
+        this.getServer().getPluginManager().registerEvents(new TimerSystem(), this);
 
 
 
     }
 
+    public static JavaPlugin getInstance(){return plugin;}
+    public static CustomConfig getDataYml(){return data;}
+    public static CustomConfig getTimeYml(){return time;}
     @Override
     public void onDisable() {
         // Plugin shutdown logic
