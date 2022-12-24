92214af117bb0578d51d40c6b77457d7ecee3c69
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-12-24 15:28:48 +0900

    感圧版データの保持により、計算量削減

--- src/main/java/silverassist/mazecreate/TimerSystem.java
+++ src/main/java/silverassist/mazecreate/TimerSystem.java
@@ -8,16 +8,19 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static silverassist.mazecreate.Function.broadCast;
 import static silverassist.mazecreate.Function.sendPrefixMessage;
 
 public class TimerSystem implements Listener {
     static Map<UUID, List<String>> timeMemo = new HashMap<>();
-    private Map<Player, Location> coolDown = new HashMap<>();
+    private HashMap<Player, Location> coolDown = new HashMap<>();
+    private HashMap<Location,String> plateMemo = new HashMap<>();
     @EventHandler
     public void playerMoveEvent(PlayerInteractEvent e){
         if(e.getAction() != Action.PHYSICAL)return;
@@ -30,17 +33,31 @@ public class TimerSystem implements Listener {
         Material block = loc.getBlock().getType();
         if(block!= Material.DIAMOND_BLOCK && block!=Material.GOLD_BLOCK)return;
         FileConfiguration data = MazeCreate.getDataYml().getConfig();
-        AtomicReference<String> AtoID = new AtomicReference<>("");
-        data.getKeys(false).forEach( id -> {
-            if (!p.getWorld().getUID().equals(UUID.fromString(data.getString(id + ".world"))))return;
-            List<Float> ls = data.getFloatList(id + ".start");
-            List<Float> le = data.getFloatList(id + ".end");
-            if(ls.get(0) > loc.getX() || le.get(0) < loc.getX())return;
-            if(ls.get(1) > loc.getY() + 1 || le.get(1) < loc.getY()+1)return;
-            if(ls.get(2) > loc.getZ() || le.get(2) < loc.getZ())return;
-            AtoID.set(id);
-        });
-        if(AtoID.get().equals(""))return;
+
+        String id;
+        if(plateMemo.containsKey(loc)){
+            id = plateMemo.get(loc);
+        }else {
+            AtomicReference<String> AtoID = new AtomicReference<>("");
+            data.getKeys(false).forEach(key-> {
+                if (!p.getWorld().getUID().equals(UUID.fromString(data.getString(key + ".world")))) return;
+                List<Float> ls = data.getFloatList(key + ".start");
+                List<Float> le = data.getFloatList(key + ".end");
+                if (ls.get(0) > loc.getX() || le.get(0) < loc.getX()) return;
+                if (ls.get(1) > loc.getY() + 1 || le.get(1) < loc.getY() + 1) return;
+                if (ls.get(2) > loc.getZ() || le.get(2) < loc.getZ()) return;
+                AtoID.set(key);
+            });
+            id = AtoID.get();
+            if(id.equals(""))return;
+            plateMemo.put(loc,id);
+            Bukkit.getScheduler().runTaskLater(MazeCreate.getInstance(), new Runnable() {
+                @Override
+                public void run() {
+                    plateMemo.remove(loc);
+                }
+            },20*30);
+        }
         e.setCancelled(true);
         UUID u = p.getUniqueId();
         coolDown.put(p,loc);
@@ -51,7 +68,6 @@ public class TimerSystem implements Listener {
             }
         },20*5);
 
-        String id = AtoID.get();
         if(block == Material.DIAMOND_BLOCK) {
             timeMemo.remove(u);
             timeMemo.put(u,List.of(id,String.valueOf(System.currentTimeMillis())));
@@ -71,10 +87,17 @@ public class TimerSystem implements Listener {
             p.teleport( home == null ? p.getWorld().getSpawnLocation() : home);
 
             FileConfiguration time = MazeCreate.getTimeYml().getConfig();
-            long min = time.getLong(id+"."+u) == 0 ?  9223372036854775807l : time.getLong(id+"."+u);
+            long min = time.getLong(id+"."+u) == 0 ?  Long.MAX_VALUE : time.getLong(id+"."+u);
             time.set(id+"."+u, Math.min(clearTime, min));
             MazeCreate.getTimeYml().saveConfig();
         }
 
+    }
+
+    @EventHandler
+    public void plateBreak(BlockBreakEvent e){
+        Location loc = e.getBlock().getLocation().add(0,-1,0);
+        if(!plateMemo.containsKey(loc))return;
+        plateMemo.remove(loc);
     }
 }
