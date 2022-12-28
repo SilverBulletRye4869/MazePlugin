912ecf82e25e5400476d0d72e90e80d4f7b91009
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-12-28 12:47:37 +0900

    バグ修正

--- src/main/java/silverassist/mazecreate/Command.java
+++ src/main/java/silverassist/mazecreate/Command.java
@@ -19,7 +19,6 @@ import static silverassist.mazecreate.Function.*;
 
 public class Command implements CommandExecutor {
 
-
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
         if(!(sender instanceof Player))return true;
@@ -32,8 +31,10 @@ public class Command implements CommandExecutor {
         ItemStack item;
         ItemMeta meta;
         FileConfiguration data = MazeCreate.getDataYml().getConfig();
+        World world;
 
         switch (args[0]){
+            //===============================================================================領域指定斧取得
             case "wand":
                 if(!p.isOp())return true;
                 item = new ItemStack(Material.DIAMOND_AXE);
@@ -43,6 +44,7 @@ public class Command implements CommandExecutor {
                 item.setItemMeta(meta);
                 p.getInventory().addItem(item);
                 return true;
+            //================================================================================迷路生成
             case "create":
                 if(!p.isOp())return true;
                 if(args.length<4)return true;
@@ -51,6 +53,7 @@ public class Command implements CommandExecutor {
                     return true;
                 }
 
+                //壁にするブロックを見る
                 Material mate;
                 try {
                     mate = Material.valueOf(args[3]);
@@ -58,19 +61,25 @@ public class Command implements CommandExecutor {
                     sendPrefixMessage(p,"§cアイテムが適切ではありません");
                     return true;
                 }
+                if(!mate.isBlock()){
+                    sendPrefixMessage(p,"§cそれはブロックではありません");
+                    return true;
+                }
 
+                //斧判定
                 item = p.getInventory().getItemInMainHand();
                 if (item.getType() != Material.DIAMOND_AXE) return true;  //ダイヤの斧か
                 meta = item.getItemMeta();
                 if (meta == null) return true;
                 if (!meta.getDisplayName().equals(MAZEWAND)) return true;  //名前が等しいか
-                //=================================================================斧が正常なものか判定
+                //斧が正常なものか判定
                 List<String> lore = meta.getLore();
                 if (lore == null) return true;
                 if (lore.size() != 3 || !lore.get(2).equals(p.getUniqueId().toString())) {
                     sendPrefixMessage(p, "§c§l斧が正常ではありません。再度斧を取得してください");
                     return true;
                 }
+
                 //指定されているか
                 lore = meta.getLore();
                 if(lore.get(1).contains("§6§l左クリックで指定")){
@@ -81,6 +90,7 @@ public class Command implements CommandExecutor {
                     sendPrefixMessage(p,"§d§l終了位置が指定されていません");
                     return true;
                 }
+
                 //各座標の最少位置、最大位置を登録
                 String[] locS = lore.get(0).replace("§f§l開始位置: ","").split(",");  //最少位置
                 String[] locE = lore.get(1).replace("§f§l終了位置: ","").split(",");  //最大位置
@@ -97,8 +107,9 @@ public class Command implements CommandExecutor {
                     }
                 }
 
-                int width = args.length > 4 && args[4].matches("\\d+") ? Integer.parseInt(args[2]) : 1;
-                if((locRegister[1][0] - locRegister[0][0])%(width+1)!=0){
+                //迷路の情報を作成
+                int width = args.length > 4 && args[4].matches("\\d+") ? Integer.parseInt(args[4]) : 1;  //幅を生成
+                if((locRegister[1][0] - locRegister[0][0])%(width+1)!=0){  //道幅による横幅調整
                     locRegister[1][0]-=(locRegister[1][0] - locRegister[0][0])%(width+1);
                     if(locRegister[1][0]==0){
                         sendPrefixMessage(p,"§cdxが小さすぎます");
@@ -106,7 +117,7 @@ public class Command implements CommandExecutor {
                     }
                     sendPrefixMessage(p,"§c§nn(間隔+1)+1=Δx§r§cを満たす自然数nが存在しなかったのでxが若干縮小されました");
                 }
-                if((locRegister[1][2] - locRegister[0][2])%(width+1)!=0){
+                if((locRegister[1][2] - locRegister[0][2])%(width+1)!=0){  //道幅による縦幅調整
                     locRegister[1][2]-=(locRegister[1][2] - locRegister[0][2])%(width+1);
                     if(locRegister[1][2]==0){
                         sendPrefixMessage(p,"§cdxが小さすぎます");
@@ -115,13 +126,16 @@ public class Command implements CommandExecutor {
                     sendPrefixMessage(p,"§c§nn(間隔+1)+1=Δz§r§cを満たす自然数nが存在しなかったのでzが若干縮小されました");
                 }
 
-                World world = Bukkit.getWorld(locS[0]);
-                if(args[2].equals("0"))ExtendWall.createMaze(locRegister, world, mate, width);
-                else if(args[2].charAt(0)=='1') new StickKnockDown().createMaze(locRegister,world, mate, width, args[2].contains("r"));
+                //迷路を生成
+                world = Bukkit.getWorld(locS[0]);
+                if(args[2].equals("0"))new ExtendWall(width).createMaze(locRegister, world, mate);  //壁伸ばし法
+                else if(args[2].charAt(0)=='1') new StickKnockDown(width,args[2].contains("r")).createMaze(locRegister,world, mate);  //棒倒し法
                 else{
                     sendPrefixMessage(p, "§c§l不明な生成タイプ番号です");
                     return true;
                 }
+
+                //ymlに記録
                 data.set(args[1]+".world",world.getUID().toString());
                 data.set(args[1]+".start",List.of(locRegister[0][0],locRegister[0][1],locRegister[0][2]));
                 data.set(args[1]+".end",List.of(locRegister[1][0],locRegister[1][1],locRegister[1][2]));
@@ -129,8 +143,7 @@ public class Command implements CommandExecutor {
                 break;
 
             case "sethome":
-                if(!p.isOp())return true;
-                if(args.length<2)return true;
+                if(!p.isOp() || args.length<2)return true;
                 if(data.get(args[1])==null){
                     sendPrefixMessage(p,"§c§lそのidの迷路が見つかりません");
                     return true;
@@ -152,8 +165,7 @@ public class Command implements CommandExecutor {
                 return true;
 
             case "delete":
-                if(!p.isOp())return true;
-                if(args.length<2)return true;
+                if(!p.isOp() || args.length<2)return true;
                 if(data.get(args[1])==null){
                     sendPrefixMessage(p,"§c§lそのidの迷路が見つかりません");
                     return true;
@@ -161,6 +173,7 @@ public class Command implements CommandExecutor {
                 world = Bukkit.getWorld(UUID.fromString(data.getString(args[1]+".world")));
                 List<Float> s = data.getFloatList(args[1]+".start");
                 List<Float> e = data.getFloatList(args[1]+".end");
+                //O(N^3)だからなんとかしたい
                 for(int i = s.get(0).intValue(); i<=e.get(0).intValue();i++){
                     for(int j =s.get(1).intValue();j<=e.get(1).intValue();j++){
                         for(int k = s.get(2).intValue();k<e.get(2).intValue();k++)world.getBlockAt(i,j,k).setType(Material.AIR);
@@ -173,4 +186,6 @@ public class Command implements CommandExecutor {
         MazeCreate.getDataYml().saveConfig();
         return true;
     }
+
+
 }
